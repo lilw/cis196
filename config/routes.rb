3783c5964583e378590ecd0039e85ecd340d1226
Lily Wang <lilyw.416@gmail.com>
2013-02-06 11:06:09 -0500

    actually fixed controller

--- config/routes.rb
+++ config/routes.rb
@@ -57,7 +57,7 @@ Newapp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'housing#index'
+  root :to => 'housings#index'
 
   # See how all your routes lay out with "rake routes"
 
